5620312c454c0c0faa05ba11bf4c37163b984192
mossah10 <mossah19@gmail.com>
2023-11-19 22:59:12 -0600

    Finished hourglass portion

---
+++ Scrumdinger/MeetingView.swift
@@ -0,0 +1,28 @@
+import SwiftUI
+    
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            VStack {
+                ProgressView(value: 5, total: 15)
+                HStack{
+                    VStack {
+                        Text("Seconds Elapsed")
+                        Label("300", systemImage: "hourglass.tophalf.fill")
+                    }
+                    VStack {
+                        Text("Seconds Remaining")
+                        Label("600", systemImage: "hourglass.bottomhalf.fill")
+                    }
+                }
+            }
+        }
+        .padding()
+    }
+}
+    
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
